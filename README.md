77c037cc76145f4c8350d2c4b11f67543decab36
Brad Christie <bch@sitecore.net>
2019-07-23 16:26:21 -0400

    Added README

---
+++ README.md
@@ -0,0 +1,18 @@
+Sphinx on Windows (w/ Docker)
+=============================
+
+Don't want to install python? Still want to develop using Sphinx documentation? This repo's for you.
+
+Leveraging the python container, we can create, build and develop sphinx documentation.
+
+# .\Sphinx.ps1 -Create -Project "Example" -Author "Me" -Version "1.0.0"
+
+Creates an "Example" project, authored by "Me" defined as version "1.0.0"
+
+# .\Sphinx.ps1 -Build [-SourceDir .] [-OutputDir _build]
+
+Builds the sphinx documentation.
+
+# .\Sphinx -Serve [-Port 3000] [-SourceDir .] [-OutputDir _build]
+
+Builds and hosts the sphinx documentation, while also serving it (and watching for changes).
--- Sphinx.ps1
+++ Sphinx.ps1
@@ -12,9 +12,19 @@ Param(
   [ValidateNotNullOrEmpty()]
   [version]$Version = "1.0.0"
   ,
+
   [Parameter(ParameterSetName = "__Build")]
   [switch]$Build
   ,
+  [Parameter(ParameterSetName = "__Build")]
+  [Parameter(ParameterSetName = "__Serve")]
+  [string]$SourceDir = "."
+  ,
+  [Parameter(ParameterSetName = "__Build")]
+  [Parameter(ParameterSetName = "__Serve")]
+  [string]$OutputDir = "_build"
+  ,
+
   [Parameter(ParameterSetName = "__Serve")]
   [switch]$Serve
   ,
@@ -42,12 +52,12 @@ Switch ($PSCmdlet.ParameterSetName) {
   "__Build" {
     Write-Output "Building..."
     docker.exe run -it --volume "${PSScriptRoot}:C:\project" $sphinxContainerName `
-      sphinx-build -b html . _build
+      sphinx-build -b html $SourceDir $OutputDir
   }
   "__Serve" {
     Write-Output "Serving..."
     docker.exe run -it --volume "${PSScriptRoot}:C:\project" --publish "3000:${Port}" $sphinxContainerName `
-      sphinx-autobuild --host 0.0.0.0 --port $Port --ignore *.ps1 --ignore .dockerignore --ignore Dockerfile --ignore .git/ . _build
+      sphinx-autobuild --host 0.0.0.0 --port $Port --ignore *.ps1 --ignore .dockerignore --ignore Dockerfile --ignore .git/ $SourceDir $OutputDir
   }
 }
 <#
